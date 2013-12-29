7f3c1ca09c67f597e1f1f03cc47c8fc0bf00ee48
Evan Lloyd <eulloyd@gmail.com>
2013-12-29 15:31:01 +0900

    Generates Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tipy::Application.config.secret_key_base = '8fd572152ee99f7fecfbab1b537d787ac92d74eaea5c8cc3ac8b8275377db2b5efe07648bad151b92d1fadb6ca77e3c11b0f412dc60139a4e97fbd67e0359ccd'
